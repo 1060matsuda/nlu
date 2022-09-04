db022d81f31b37fc1940b3e1b63aebfefb89a8c7
nayu_labpc <matsuda-nayuta775@g.ecc.u-tokyo.ac.jp>
2022-09-04 21:44:04 +0900

    stat processing completed

--- common_files/ave_stderr.py
+++ common_files/ave_stderr.py
@@ -1,4 +1,6 @@
 # %%
+# Run this file at manytimes/ directory that contains manytimes/default/ directory.
+
 import yaml
 from cProfile import label
 import os
@@ -19,16 +21,26 @@ from python_modules import FFT_utility as fu
 # %%
 file_to_process = "beta_sum_corr.txt"
 dir_index_start = int(1)
-dir_index_end = int(100)
+dir_index_end = int(50)
 dirs_num = dir_index_end-dir_index_start+1
 values_array = np.zeros(dirs_num)
 print("Fetching values from "+str(dirs_num)+" directories...")
 
 for dir_index in range(dir_index_start, dir_index_start+dirs_num):
     with open("./"+str(dir_index)+"/"+file_to_process) as raw_data:
-        str_raw_data = raw_data.read()  
+        str_raw_data = raw_data.read()
     # correlate dir_index and values_array's index
-    value_array_index = dir_index - dir_index_start
-    values_array[value_array_index] = float(str_raw_data)
+    values_array_index = dir_index - dir_index_start
+    values_array[values_array_index] = float(str_raw_data)
 
+# %%
+with open("./default/config.yaml", "r") as yml:
+    config = yaml.safe_load(yml)
+defects_rate = config["defects_rate"]
+beta_mean = np.mean(values_array)
+beta_err = np.std(values_array, ddof=1)/np.sqrt(dirs_num)
+with open("beta_stat.csv", "w") as b:
+    writer = csv.writer(b, delimiter=",")
+    writer.writerow([defects_rate, beta_mean, beta_err])
+print(defects_rate, beta_mean, beta_err)
 # %%
