4bfb144aa3612adcb4d946477dc06910af25f259
nayu.csc <matsuda-nayuta775@g.ecc.u-tokyo.ac.jp>
2022-10-15 22:04:28 +0900

    multidetec

--- common_files/yaml_to_lammps_nrb_mix.py
+++ common_files/yaml_to_lammps_nrb_mix.py
@@ -11,10 +11,10 @@ timestep = config["timestep"]
 
 # %%
 script = "#!/bin/bash\n"
-script=script+"cp $MYWORK/common_files/in.lammps.nrb.mix ./mod_in.lammps.nrb.mix\n"
-script=script+"sed -i -e 's/TIMESTEP_REPLACE_YAML/" + str(timestep)+"/g' ./mod_in.lammps.nrb.mix\n"
-script=script+"sed -i -e 's/F1_REPLACE_YAML/" + str(f1)+"/g' ./mod_in.lammps.nrb.mix\n"
-script=script+"sed -i -e 's/F2_REPLACE_YAML/" + str(f2)+"/g' ./mod_in.lammps.nrb.mix"
+script=script+"cp $MYWORK/common_files/in.lammps.detec.nrb.mix ./mod_in.lammps.detec.nrb.mix\n"
+script=script+"sed -i -e 's/TIMESTEP_REPLACE_YAML/" + str(timestep)+"/g' ./mod_in.lammps.detec.nrb.mix\n"
+script=script+"sed -i -e 's/F1_REPLACE_YAML/" + str(f1)+"/g' ./mod_in.lammps.detec.nrb.mix\n"
+script=script+"sed -i -e 's/F2_REPLACE_YAML/" + str(f2)+"/g' ./mod_in.lammps.detec.nrb.mix"
 
 # %%
 shell_script_file_name = "lammps_script_modifier_temp.sh"
