d744ed69cf1f2bebfaf28df3ddd0c11710586fb1
nayu.csc <matsuda-nayuta775@g.ecc.u-tokyo.ac.jp>
2022-12-12 02:53:32 +0900

    shg_ppt

---
+++ shg/ppt_freq/500G/1/csc_32nodes.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --account=HEDISINT
+#SBATCH --ntasks=2560
+#SBATCH --cpus-per-task=2
+#SBATCH --partition=L
+#SBATCH --time=23:59:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user="matsuda-nayuta775@g.ecc.u-tokyo.ac.jp"
+#SBATCH --job-name=test_cu
+
+#for i in `seq 50`
+#do
+#cd $i
+#srun /project/HEDISINT/lammps/lammps/src/lmp_mpi -in mod_in.lammps.mix
+#cd ..
+#done
+
+srun /project/HEDISINT/lammps/lammps/src/lmp_mpi -in mod_in.lammps.nonrb.shg
